68c5e37c407587d87d4e8d8b9ac470a324a4f338
Ravi Kumar <ravik.kr02@gmail.com>
2013-10-14 17:14:25 -0400

    Changes on SiteMap2Class
    this is done, started to work on other part.

---
+++ aoSEOSiteMap/SiteMap2Class.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Contensive.BaseClasses;
+
+namespace aoSEOSiteMap
+{
+    public class SiteMap2Class : AddonBaseClass
+    {
+        private const string MapFileName = "seoSiteMap.xml";
+        public override object Execute(CPBaseClass cp)
+        {
+            try
+            {
+                CPCSBaseClass cs = cp.CSNew();
+                CPFileBaseClass cpFile = cp.File;
+                cp.Response.Clear();
+                cp.Response.SetType("text/XML");
+                return cp.File.Read(cp.Site.PhysicalFilePath + MapFileName);
+             }
+            catch
+            {
+                cp.Site.ErrorReport("SiteMap2Class");
+                return string.Empty;
+            }
+         }
+
+     }
+}
---
+++ aoSEOSiteMap/SiteMapClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Contensive.BaseClasses;
+
+namespace aoSEOSiteMap
+{
+    public class SiteMapClass : AddonBaseClass
+    {
+        public override object Execute(CPBaseClass CP)
+        {
+            CPCSBaseClass cs = CP.CSNew();
+            
+        }
+    }
+}
