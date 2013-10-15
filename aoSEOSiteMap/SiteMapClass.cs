4c3977b5d21563352e3ef07949b57e694abc6c37
Ravi Kumar <ravik.kr02@gmail.com>
2013-10-15 17:18:01 -0400

    SiteMapClass
    Working on SiteMapClass

--- aoSEOSiteMap/SiteMapClass.cs
+++ aoSEOSiteMap/SiteMapClass.cs
@@ -8,10 +8,21 @@ namespace aoSEOSiteMap
 {
     public class SiteMapClass : AddonBaseClass
     {
+        private int PageNotFoundID;
         public override object Execute(CPBaseClass CP)
         {
             CPCSBaseClass cs = CP.CSNew();
-            
+            CP.Response.Clear();
+            CP.Response.SetType("text/XML");
+            PageNotFoundID = CP.Utils.EncodeInteger(CP.Site.GetText("PAGENOTFOUNDPAGEID"));
+            //String stream = "<?xml version=""1.0"" encoding=""UTF-8""?>"  + System.Environment.NewLine;
+            //stream = stream + "<urlset xmlns=""http://www.sitemaps.org/schemas/sitemap/0.9"">" + System.Environment.NewLine;
+            CP.Site.Name("ccCSrvr3.ContentServerClass");
+            CP.Site.Name(CP.Response.SetType);
+            if (CP.Response.SetStatus = 2)
+            { 
+
+           }
         }
     }
 }
