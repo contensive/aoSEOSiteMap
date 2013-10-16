45d700e773a766fdc0190b0902163ce5fbe2a64f
Ravi Kumar <ravik.kr02@gmail.com>
2013-10-16 17:07:19 -0400

    SiteMapClass & GenerationClass
    SiteMapClass is finished and started on GenerationClass.

--- aoSEOSiteMap/GenerationClass.cs
+++ aoSEOSiteMap/GenerationClass.cs
@@ -6,7 +6,25 @@ using Contensive.BaseClasses;
 
 namespace aoSEOSiteMap
 {
-    public class GenerationClass
-    {
-    }
+    //public class GenerationClass: AddonBaseClass
+    //{
+    //    public override object Execute(CPBaseClass CP)
+    //    {
+    //        try
+    //        {
+    //            isAliasing = CP.Utils.EncodeBoolean(CP.Site.GetText("ALLOWLINKALIAS","0"));
+    //            PageNotFoundID = CP.Utils.EncodeInteger(CP.Site.GetText("PAGENOTFOUNDPAGEID", "0"));
+    //            DefaultPage = CP.Site.GetText("SERVERPAGEDEFAULT", "0");
+    //            //xmlContent = "";
+    //            return xmlContent="";
+    //        }
+    //        catch(Exception ex)
+    //        {
+    //            CP.Site.ErrorReport(ex, "Unexpected trap");
+    //        }
+    //    }
+    //    private Boolean isAliasing;
+    //    private long PageNotFoundID;
+    //    private string DefaultPage, xmlContent;
+    //}
 }
