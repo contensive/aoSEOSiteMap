0cf9c4f147d9fcfbcc6b68de6b3f63dd04f1db8c
Ravi Kumar <ravik.kr02@gmail.com>
2013-10-21 17:25:09 -0400

    Changes Done
    Lets review it and let me know the changes.

--- aoSEOSiteMap/SiteMapClass.cs
+++ aoSEOSiteMap/SiteMapClass.cs
@@ -12,41 +12,41 @@ namespace aoSEOSiteMap
         {
             try
             {
-                CPCSBaseClass cs = cp.CSNew();
-                cp.Response.Clear();
-                cp.Response.SetType("text/XML");
-                pageNotFoundID = cp.Doc.GetInteger("PAGENOTFOUNDPAGEID");
-                stream = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-                stream += "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
-                returnValue = cp.Site.Name;
-                CPContentBaseClass cc = cp.Content;
+                return "";
+            //    CPCSBaseClass cs = cp.CSNew();
+            //    cp.Response.Clear();
+            //    cp.Response.SetType("text/XML");
+            //    pageNotFoundID = cp.Doc.GetInteger("PAGENOTFOUNDPAGEID");
+            //    stream = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+            //    stream += "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
+            //    returnValue = cp.Site.Name;
+            //    CPContentBaseClass cc = cp.Content;
                        
-                for (pointer = 0; pointer <= count; pointer++)
-                {
-                    //PageID = KmaEncodeNumber(PCC(PCC_ID, Pointer))//VB6 Code
-                    // pageID= cp.Utils.EncodeNumber(PCC(PCC_ID, pointer));//C# Code
-                    if (pageNotFoundID != pageID)
-                    {
-                        pageLink = cp.Utils.EncodeUrl(cc.GetPageLink((int)pageID, "", true));
-                        if (pageLink.IndexOf("://", 1, StringComparison.CurrentCultureIgnoreCase) == 0)
-                        {
-                            pageLink = "http://" + cp.Request.Host + pageLink;
-                        }
-                        pageLink = pageLink.Replace("&", "&amp;");
-                        // ModifiedDate = KmaEncodeDate(PCC(PCC_ModifiedDate, Pointer))//VB6 Code
-                       // modifiedDate = cp.Utils.EncodeDate(PCC(PCC_ModifiedDate, pointer));//C# Code
-                        stream += "<url>\n";
-                        stream += "<loc>" + pageLink + "</loc>\n";
-                        if (modifiedDate.TimeOfDay != new TimeSpan(12, 00, 00))
-                        {
-                            stream += "<lastmod>" + GetDate(cp, modifiedDate) + "</lastmod>\n";
-                        }
-                        stream += "</url>\n";
-                    }
-                }
+            //    for (pointer = 0; pointer <= count; pointer++)
+            //    {
+            //        //PageID = KmaEncodeNumber(PCC(PCC_ID, Pointer))//VB6 Code
+            //        // pageID= cp.Utils.EncodeNumber(PCC(PCC_ID, pointer));//C# Code
+            //        if (pageNotFoundID != pageID)
+            //        {
+            //            pageLink = cp.Utils.EncodeUrl(cc.GetPageLink((int)pageID, "", true));
+            //            if (pageLink.IndexOf("://", 1, StringComparison.CurrentCultureIgnoreCase) == 0)
+            //            {
+            //                pageLink = "http://" + cp.Request.Host + pageLink;
+            //            }
+            //            pageLink = pageLink.Replace("&", "&amp;");
+            //            // ModifiedDate = KmaEncodeDate(PCC(PCC_ModifiedDate, Pointer))//VB6 Code
+            //            //modifiedDate = cp.Utils.EncodeDate(PCC(PCC_ModifiedDate, pointer));//C# Code
+            //            stream += "<url>\n";
+            //            stream += "<loc>" + pageLink + "</loc>\n";
+            //            if (modifiedDate.TimeOfDay != new TimeSpan(12, 00, 00))
+            //            {
+            //                stream += "<lastmod>" + GetDate(cp, modifiedDate) + "</lastmod>\n";
+            //            }
+            //            stream += "</url>\n";
+            //        }
+                //}
 
-                stream += "</urlset>\n";
-                return stream;
+                //stream += "</urlset>\n";
             }
             catch (Exception ex)
             {
@@ -56,6 +56,7 @@ namespace aoSEOSiteMap
 
         }
 
+        
         private string GetDate(CPBaseClass cp, DateTime givenDate)
         {
             try
